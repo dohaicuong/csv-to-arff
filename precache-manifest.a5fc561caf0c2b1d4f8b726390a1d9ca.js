da5ded4bf961cbb76ed9ccb8b7ceee038636d8c1
dohaicuong <beatyshot@gmail.com>
2020-07-28 13:52:22 +0000

    Deploying to gh-pages from  @ 44ce68ac5fb414fda18a93d1c3b7fd04570b8031 🚀

---
+++ precache-manifest.a5fc561caf0c2b1d4f8b726390a1d9ca.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5634ad5f42f91c00bf75a54b6e20760b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ba03d7218529bb21bab1",
+    "url": "/static/js/2.da16a61a.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.da16a61a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f76137eeada8a3db6f97",
+    "url": "/static/js/main.30c9069f.chunk.js"
+  },
+  {
+    "revision": "37bedb4d9f8cf2710ff0",
+    "url": "/static/js/runtime-main.b13fa237.js"
+  }
+]);
